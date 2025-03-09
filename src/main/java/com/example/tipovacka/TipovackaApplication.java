699b74799e52e11432657a21f5503f6f2132e197
Michal Němeček <michal.nemecek@o2its.cz>
2025-03-09 22:50:06 +0100

    UTF8 implementation for OpenAPI

--- src/main/java/com/example/tipovacka/TipovackaApplication.java
+++ src/main/java/com/example/tipovacka/TipovackaApplication.java
@@ -3,13 +3,25 @@ package com.example.tipovacka;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition
 @SpringBootApplication
-public class TipovackaApplication {
+@Configuration
+public class TipovackaApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TipovackaApplication.class, args);
 	}
 
+	@Bean
+	public CharacterEncodingFilter characterEncodingFilter() {
+		CharacterEncodingFilter filter = new CharacterEncodingFilter();
+		filter.setEncoding("UTF-8");
+		filter.setForceEncoding(true);
+		return filter;
+	}
 }
